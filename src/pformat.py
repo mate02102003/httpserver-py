0e969ca8070afcebecca5f19397e19d85afd4876
mate02102003 <mate02102003@gmail.com>
2024-11-03 15:19:11 +0100

    fully implemented for the necessary built-in types

--- src/pformat.py
+++ src/pformat.py
@@ -1,8 +1,40 @@
+"""
+Pretty format
+
+This module serves as pretty formatter for the built-in classes in python
+"""
+
+def pformat_int(__int: int) -> str:
+    return f"{__int:_}"
+
+def pformat_complex(__complex: complex) -> str:
+    return f"{__complex.real} {"+" if __complex.imag >= 0 else "-"} {abs(__complex.imag)}i"
+
 def pformat_tuple(__tuple: tuple, indent: int = 0) -> str:
     acc = "(\n"
     for value in __tuple:
             acc += f"{" " * indent} {pformat(__tuple[value], indent + len(str(value)) + 3)}\n"
     acc += " " * indent + ")"
+    return acc
+
+def pformat_list(__list: list, indent: int = 0) -> str:
+    acc = "[\n"
+    for value in __list:
+            acc += f"{" " * indent} {pformat(__list[value], indent + len(str(value)) + 3)}\n"
+    acc += " " * indent + "]"
+    return acc
+
+def pformat_set(__set: set, indent: int = 0) -> str:
+    acc = "{\n"
+    for value in __set:
+            acc += f"{" " * indent} {pformat(__set[value], indent + len(str(value)) + 3)}\n"
+    acc += " " * indent + "}"
+
+def pformat_frozenset(__fset: frozenset, indent: int = 0) -> str:
+    acc = "frozenset{\n"
+    for value in __fset:
+            acc += f"{" " * indent} {pformat(__fset[value], indent + len(str(value)) + 3)}\n"
+    acc += " " * indent + "}"
     return acc
 
 def pformat_dict(__dict: dict, indent: int = 0) -> str:
@@ -15,8 +47,18 @@ def pformat_dict(__dict: dict, indent: int = 0) -> str:
 def pformat(__obj: object, indent: int = 0) -> str:
     o_type = type(__obj)
 
+    if issubclass(o_type, int):
+        return pformat_int(__obj)
+    if issubclass(o_type, complex):
+        return pformat_complex(__obj)
     if issubclass(o_type, tuple):
         return pformat_tuple(__obj, indent)
+    if issubclass(o_type, list):
+        return pformat_list(__obj, indent)
+    if issubclass(o_type, set):
+        return pformat_set(__obj, indent)
+    if issubclass(o_type, frozenset):
+        return pformat_frozenset(__obj, indent)
     if issubclass(o_type, dict):
         return pformat_dict(__obj, indent)
     return repr(__obj)
@@ -31,7 +73,7 @@ class PP_Repr:
 
         indent = " " * self.format_indent
         formated_attrs = self.format_join.join(
-             f"{indent}{attr}: {self.format_func(getattr(self, attr),self.format_indent)}"
+             indent + f"{attr}: {self.format_func(getattr(self, attr), self.format_indent)}"
              for attr in dir(self) if not attr.startswith("_") and getattr(PP_Repr, attr, None) is None
         )
 
