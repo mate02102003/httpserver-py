70be02cd561814f02d31bd4b6191af03b45f5133
mate02102003 <mate02102003@gmail.com>
2024-11-04 20:16:39 +0100

    made type hinting more accurate

--- src/httpconstants.py
+++ src/httpconstants.py
@@ -1,12 +1,14 @@
-CHAR: tuple[str]    = tuple(octet.to_bytes().decode("ascii") for octet in range(128))
-UPALPHA: tuple[str] = tuple(filter(str.isupper, CHAR))
-LOALPHA: tuple[str] = tuple(filter(str.islower, CHAR))
-ALPHA: tuple[str]   = UPALPHA + LOALPHA
-DIGIT: tuple[str]   = tuple(map(str, range(10)))
-CTL: tuple[str]     = CHAR[0:32] + CHAR[127]
-CR: str             = '\r'
-LF: str             = '\n'
-SP: str             = ' '
-HT: str             = '\t'
-DQ: str             = '"'
-CRLF: str           = CR + LF
+from typing import Final
+
+CHAR: Final[tuple[str]]    = tuple(octet.to_bytes().decode("ascii") for octet in range(128))
+UPALPHA: Final[tuple[str]] = tuple(filter(str.isupper, CHAR))
+LOALPHA: Final[tuple[str]] = tuple(filter(str.islower, CHAR))
+ALPHA: Final[tuple[str]]   = UPALPHA + LOALPHA
+DIGIT: Final[tuple[str]]   = tuple(map(str, range(10)))
+CTL: Final[tuple[str]]     = CHAR[0:32] + CHAR[127]
+CR: Final[str]             = '\r'
+LF: Final[str]             = '\n'
+SP: Final[str]             = ' '
+HT: Final[str]             = '\t'
+DQ: Final[str]             = '"'
+CRLF: Final[str]           = CR + LF
