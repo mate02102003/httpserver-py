4f844f0e31ca28aef7446d2bf9ac9eb6f8421556
mate02102003 <mate02102003@gmail.com>
2024-11-04 19:58:36 +0100

    changed how request version is stored

--- src/httprequest.py
+++ src/httprequest.py
@@ -8,11 +8,11 @@ from httpheaders import HTTPHeaders
 
 @dataclasses.dataclass(repr=False)
 class HTTPRequest(PP_Repr):
-    method: HTTPMethod   = dataclasses.field(init=False)
-    target: str          = dataclasses.field(init=False)
-    version: str         = dataclasses.field(default="HTTP/1.1")
-    headers: HTTPHeaders = dataclasses.field(default_factory=HTTPHeaders)
-    body: str            = dataclasses.field(default="\r\n")
+    method: HTTPMethod       = dataclasses.field(init=False)
+    target: str              = dataclasses.field(init=False)
+    version: tuple[int, int] = dataclasses.field(default=(1, 1))
+    headers: HTTPHeaders     = dataclasses.field(default_factory=HTTPHeaders)
+    body: str                = dataclasses.field(default="\r\n")
 
     @staticmethod
     def parse_request(request: str) -> "HTTPRequest":
@@ -22,9 +22,11 @@ class HTTPRequest(PP_Repr):
 
         headers, http_request.body = rest[:rest.index("")], "\r\n".join(rest[rest.index(""):])
 
-        method, http_request.target, http_request.version = request_line.split()
+        method, target, version = request_line.split()
+
         http_request.method = HTTPMethod(method)
-        http_request.target = urllib.parse.unquote(http_request.target)
+        http_request.target = urllib.parse.unquote(target)
+        http_request.version = tuple(map(int, version.split("/")[1].split(".")))
 
         for line in headers:
             header_name, header_content = line.split(':', maxsplit=1)
