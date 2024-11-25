7b90062bcdd45924c3c175e6f859283a41c36752
mate02102003 <mate02102003@gmail.com>
2024-11-25 23:13:12 +0100

    changed strings to bytes

--- src/httpconstants.py
+++ src/httpconstants.py
@@ -1,16 +1,16 @@
 from typing import Final
 
-OCTET: Final[tuple[bytes]] = tuple(octet.to_bytes() for octet in range(256))
-CHAR: Final[tuple[str]]    = tuple(map(lambda b: b.decode("ascii"), OCTET[:128]))
-UPALPHA: Final[tuple[str]] = tuple(filter(str.isupper, CHAR))
-LOALPHA: Final[tuple[str]] = tuple(filter(str.islower, CHAR))
-ALPHA: Final[tuple[str]]   = UPALPHA + LOALPHA
-DIGIT: Final[tuple[str]]   = tuple(map(str, range(10)))
-CTL: Final[tuple[str]]     = CHAR[0:32] + (CHAR[127],)
-CR: Final[str]             = '\r'
-LF: Final[str]             = '\n'
-SP: Final[str]             = ' '
-HT: Final[str]             = '\t'
-DQ: Final[str]             = '"'
-CRLF: Final[str]           = CR + LF
-HEX: Final[tuple[str]]     = "A" , "B" , "C" , "D" , "E" , "F" , "a" , "b" , "c" , "d" , "e" , "f" , *DIGIT
+OCTET: Final[tuple[bytes]]   = tuple(octet.to_bytes() for octet in range(256))
+CHAR: Final[tuple[bytes]]    = OCTET[:128]
+UPALPHA: Final[tuple[bytes]] = tuple(filter(bytes.isupper, CHAR))
+LOALPHA: Final[tuple[bytes]] = tuple(filter(bytes.islower, CHAR))
+ALPHA: Final[tuple[bytes]]   = UPALPHA + LOALPHA
+DIGIT: Final[tuple[bytes]]   = tuple((i.to_bytes() for i in range(10)))
+CTL: Final[tuple[bytes]]     = CHAR[0:32] + (CHAR[127],)
+CR: Final[bytes]             = b'\r'
+LF: Final[bytes]             = b'\n'
+SP: Final[bytes]             = b' '
+HT: Final[bytes]             = b'\t'
+DQ: Final[bytes]             = b'"'
+CRLF: Final[bytes]           = CR + LF
+HEX: Final[tuple[bytes]]     = b'A' , b'B' , b'C' , b'D' , b'E' , b'F' , b'a' , b'b' , b'c' , b'd' , b'e' , b'f' , *DIGIT
