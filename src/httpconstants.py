459607c536d1d095c49878a694e0b69432a52d48
mate02102003 <mate02102003@gmail.com>
2024-11-26 01:49:46 +0100

    moved not_found_page

--- src/httpconstants.py
+++ src/httpconstants.py
@@ -14,3 +14,19 @@ HT: Final[bytes]             = b'\t'
 DQ: Final[bytes]             = b'"'
 CRLF: Final[bytes]           = CR + LF
 HEX: Final[tuple[bytes]]     = b'A' , b'B' , b'C' , b'D' , b'E' , b'F' , b'a' , b'b' , b'c' , b'd' , b'e' , b'f' , *DIGIT
+
+NOT_FOUND_PAGE_PATH = "not_found.html"
+NOT_FOUND_PAGE = """
+                 <!DOCTYPE html>
+                 <html lang="en">
+                 <head>
+                     <meta charset="UTF-8">
+                     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                     <title>Not found</title>
+                     <link rel="icon" type="image/x-icon" href="/favicon.ico">
+                 </head>
+                 <body>
+                     <h1>404 not found!</h1>
+                 </body>
+                 </html>
+                 """
